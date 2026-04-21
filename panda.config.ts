9fdcc7dbec8542e67a2d2dbf11e8308b6e816511
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-04-21 00:01:46 -0700

    :bug: CLI not windows friendly it seems

--- panda.config.ts
+++ panda.config.ts
@@ -1,18 +1,18 @@
-import { green } from "@\\theme\\colors\\green";
-import { red } from "@\\theme\\colors\\red";
-import { neutral } from "@\\theme\\colors\\neutral";
-import { blue } from "@\\theme\\colors\\blue";
-import { animationStyles } from "@\\theme\\animation-styles";
-import { zIndex } from "@\\theme\\tokens\\z-index";
-import { shadows } from "@\\theme\\tokens\\shadows";
-import { durations } from "@\\theme\\tokens\\durations";
-import { colors } from "@\\theme\\tokens\\colors";
-import { textStyles } from "@\\theme\\text-styles";
-import { layerStyles } from "@\\theme\\layer-styles";
-import { keyframes } from "@\\theme\\keyframes";
-import { globalCss } from "@\\theme\\global-css";
-import { conditions } from "@\\theme\\conditions";
-import { slotRecipes, recipes } from "@\\theme\\recipes";
+import { green } from "@/theme/colors/green";
+import { red } from "@/theme/colors/red";
+import { neutral } from "@/theme/colors/neutral";
+import { blue } from "@/theme/colors/blue";
+import { animationStyles } from "@/theme/animation-styles";
+import { zIndex } from "@/theme/tokens/z-index";
+import { shadows } from "@/theme/tokens/shadows";
+import { durations } from "@/theme/tokens/durations";
+import { colors } from "@/theme/tokens/colors";
+import { textStyles } from "@/theme/text-styles";
+import { layerStyles } from "@/theme/layer-styles";
+import { keyframes } from "@/theme/keyframes";
+import { globalCss } from "@/theme/global-css";
+import { conditions } from "@/theme/conditions";
+import { slotRecipes, recipes } from "@/theme/recipes";
 import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
@@ -53,7 +53,7 @@ export default defineConfig({
       tokens: {
         colors: colors,
         durations: durations,
-        zIndex: zIndex
+        zIndex: zIndex,
       },
 
       semanticTokens: {
@@ -62,67 +62,68 @@ export default defineConfig({
             default: {
               value: {
                 _light: "{colors.gray.12}",
-                _dark: "{colors.gray.12}"
-              }
+                _dark: "{colors.gray.12}",
+              },
             },
 
             muted: {
               value: {
                 _light: "{colors.gray.11}",
-                _dark: "{colors.gray.11}"
-              }
+                _dark: "{colors.gray.11}",
+              },
             },
 
             subtle: {
               value: {
                 _light: "{colors.gray.10}",
-                _dark: "{colors.gray.10}"
-              }
-            }
+                _dark: "{colors.gray.10}",
+              },
+            },
           },
 
           border: {
             value: {
               _light: "{colors.gray.4}",
-              _dark: "{colors.gray.4}"
-            }
+              _dark: "{colors.gray.4}",
+            },
           },
 
           error: {
             value: {
               _light: "{colors.red.9}",
-              _dark: "{colors.red.9}"
-            }
+              _dark: "{colors.red.9}",
+            },
           },
 
           blue: blue,
           gray: neutral,
           red: red,
-          green: green
+          green: green,
         },
 
         shadows: shadows,
 
         radii: {
           l1: {
-            value: "{radii.xs}"
+            value: "{radii.xs}",
           },
 
           l2: {
-            value: "{radii.sm}"
+            value: "{radii.sm}",
           },
 
           l3: {
-            value: "{radii.md}"
-          }
-        }
-      }
+            value: "{radii.md}",
+          },
+        },
+      },
     },
   },
 
   // The output directory for your css system
   outdir: "styled-system",
+  jsxFramework: "react",
 
   globalCss: globalCss,
-  conditions: conditions
+  conditions: conditions,
 });
