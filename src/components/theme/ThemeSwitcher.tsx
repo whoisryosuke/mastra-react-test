6e14b58b76a872728de36cbaa1a42b3ee6d90e46
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-04-22 23:33:51 -0700

    :lipstick: default to dark for now...

--- src/components/theme/ThemeSwitcher.tsx
+++ src/components/theme/ThemeSwitcher.tsx
@@ -1,16 +1,23 @@
 // Example: ThemeToggle component
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui";
 import { Moon, Sun } from "lucide-react";
 
-export default function ThemeSwitcher() {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
+type ThemeMode = "light" | "dark";
 
-  const toggle = () => {
-    const next = theme === "light" ? "dark" : "light";
+export default function ThemeSwitcher() {
+  const [theme, setTheme] = useState<ThemeMode>("light");
+
+  const toggle = (newTheme: ThemeMode) => {
+    const fallbackNextTheme = theme === "light" ? "dark" : "light";
+    const next = newTheme ? newTheme : fallbackNextTheme;
     document.documentElement.className = next;
     setTheme(next);
   };
+
+  useEffect(() => {
+    toggle("dark");
+  }, []);
 
   return (
     <Button variant="outline" onClick={toggle}>
